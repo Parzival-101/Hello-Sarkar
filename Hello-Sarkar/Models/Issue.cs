45afc6a1616132e1e4ad1613fb25d4ee04fdfa28
Prabindra Sapkota <prabindrasapkota89@gmail.com>
2021-03-18 16:28:31 +0545

    issue naming convection

--- Hello-Sarkar/Models/Issue.cs
+++ Hello-Sarkar/Models/Issue.cs
@@ -8,12 +8,12 @@ namespace Hello_Sarkar.Models
     public class Issue
     {
         public int ID { get; set; }
-        public DateTime issuedDate { get; set; }
+        public DateTime IssuedDate { get; set; }
         public string Subject { get; set; }
 
         public string District { get; set; }
         public string Street { get; set; }
         public string Place { get; set; }
-        public string issueDetails { get; set; }
+        public string IssueDetails { get; set; }
     }
 }
