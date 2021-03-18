15d2bc65f946dbc83e90ca5f5976f70f8af870cd
Prabindra Sapkota <prabindrasapkota89@gmail.com>
2021-03-18 16:25:08 +0545

    Issue data model added

---
+++ Hello-Sarkar/Models/Issue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_Sarkar.Models
+{
+    public class Issue
+    {
+        public int ID { get; set; }
+        public DateTime issuedDate { get; set; }
+        public string Subject { get; set; }
+
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string Place { get; set; }
+        public string issueDetails { get; set; }
+    }
+}
