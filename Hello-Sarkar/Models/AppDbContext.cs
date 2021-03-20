6e2bb7ac78513c42eb1f2f23ba0ea994dd7a3f0b
Prabindra Sapkota <prabindrasapkota89@gmail.com>
2021-03-21 04:45:38 +0545

    Addind DbContext in Entity framework

---
+++ Hello-Sarkar/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_Sarkar.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<News> News { get; set; }
+    }
+}
