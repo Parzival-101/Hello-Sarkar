c44da7c72c2ec699a843cde03c90a4da2d78fff6
Prabindra Sapkota <prabindrasapkota89@gmail.com>
2021-03-21 18:13:01 +0545

    Adding Index and details method to the home controller

--- Hello-Sarkar/Controllers/HomeController.cs
+++ Hello-Sarkar/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Hello_Sarkar.Models;
+using Hello_Sarkar.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,7 +7,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-//using Hello_Sarkar.ViewModels;
 
 namespace Hello_Sarkar.Controllers
 {
@@ -15,16 +15,32 @@ namespace Hello_Sarkar.Controllers
      
         private readonly ILogger<HomeController> _logger;
         private readonly IIssueRepository _issueRepository;
+        private INewsRepository _newsRepository;
 
-        public HomeController(ILogger<HomeController> logger , IIssueRepository issueRepository )
+        public HomeController(ILogger<HomeController> logger , IIssueRepository issueRepository ,INewsRepository newsRepository )
         {
             _logger = logger;
             _issueRepository = issueRepository;
+            _newsRepository = newsRepository;
         }
 
+        //Get List of all news
         public IActionResult Index()
         {
-            return View();     
+            var model = _newsRepository.GetAllNews();
+            return View(model);
+        }
+
+        //Get details view of any news
+        public ActionResult Details(int? id)
+        {
+            NewsDetailsViewModel newsDetailsViewModel = new NewsDetailsViewModel()
+            {
+                News = _newsRepository.GetNews(id ??1),
+                PageTitle = "Detailed News"
+            };
+
+            return View(newsDetailsViewModel);
         }
 
         [HttpGet]
---
+++ Hello-Sarkar/ViewModels/NewsDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Hello_Sarkar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_Sarkar.ViewModels
+{
+    public class NewsDetailsViewModel
+    {
+        public News News { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
