30d09c621f5c0e953c65247adabe45fca53f5bb8
Prabindra Sapkota <prabindrasapkota89@gmail.com>
2021-03-21 07:24:59 +0545

    Implementing interface to SQL repository of news repository

---
+++ Hello-Sarkar/Models/SQLNewsRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_Sarkar.Models
+{
+    public class SQLNewsRepository : INewsRepository
+    {
+        private readonly AppDbContext context;
+
+        public SQLNewsRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public News Add(News news)
+        {
+            context.News.Add(news);
+            context.SaveChanges();
+            return news;
+        }
+
+        public News Delete(int id)
+        {
+            News news = context.News.Find(id);
+            if (news != null)
+            {
+                context.News.Remove(news);
+                context.SaveChanges();
+            }
+            return news;
+        }
+
+        public IEnumerable<News> GetAllNews()
+        {
+            return context.News;
+        }
+
+        public News GetNews(int Id)
+        {
+            return context.News.Find(Id);
+        }
+
+        public News Update(News newsChanges)
+        {
+            var news = context.News.Attach(newsChanges);
+            news.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+            return newsChanges;
+        }
+    }
+}
